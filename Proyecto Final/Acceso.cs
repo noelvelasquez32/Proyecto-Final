c94a6d61c85dc8030f988b68e088f300b7bf0ace
Wilmar Velàsquez <Wilmar Velsquez@DESKTOP-CM3QIV3>
2018-06-10 18:21:15 -0600

    mensaje contraseña incorrecta

---
+++ Proyecto Final/Acceso.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final
+{
+    class Acceso
+    {
+        private string usuario;
+        private string contraseña;
+
+
+        public string Usuario
+        {
+            get
+            {
+                return usuario;
+            }
+
+            set
+            {
+                usuario = value;
+            }
+        }
+
+        public string Contraseña
+        {
+            get
+            {
+                return contraseña;
+            }
+
+            set
+            {
+                contraseña = value;
+            }
+        }
+
+
+    }
+}
